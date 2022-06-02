e788e9325ea29a4e9bb088e9dacde29bb0821ba9
xFurciK <s222915@student.uek.krakow.pl>
2022-06-02 19:27:15 +0200

    Almost done

--- currency.py
+++ currency.py
@@ -1,42 +1,61 @@
 import requests
 import json
+from matplotlib import pyplot as plt
 
-option_choose = input('''
+option_choose = int(input('''
 Choose operation you want to do, type:
 1 to check rates
 2 to convert one currency to another
-3 to chech the average rates for some amount of days
-''')
+3 to see a exchange rate chart
+'''))
 
 def rate_check():
-    currency = input('Enter currency you want to check (GBP, PLN, EUR etc.): ')
-    url = f'https://api.nbp.pl/api/exchangerates/rates/c/{currency}/today/?format=json'
-    response = requests.get(url)
-    json_data = json.loads(response.text)
-    return round(json_data['rates'][0]['bid'], 2) 
-#print(rate_check())
+    try:
+        currency = input('Enter currency you want to check (GBP, PLN, EUR etc.): ')
+        url = f'https://api.nbp.pl/api/exchangerates/rates/c/{currency}/today/?format=json'
+        response = requests.get(url)
+        json_data = json.loads(response.text)
+        return round(json_data['rates'][0]['bid'], 2) 
+    except:
+        return 'Invalid currency shortcut'
 def converter():
-    source_currency = input('Enter currency you want to convert (EUR, GBP etc,): ')
-    source_currency_amount = int(input('Enter amount of currency you want to convert: '))
-    target_currency = input('Enter currency in which you want the result (GBP, EUR etc.): ')
+    try:
+        source_currency = input('Enter currency you want to convert (EUR, GBP etc,): ')
+        source_currency_amount = int(input('Enter amount of currency you want to convert: '))
+        target_currency = input('Enter currency in which you want the result (GBP, EUR etc.): ')
 
-    source_url = f'https://api.nbp.pl/api/exchangerates/rates/c/{source_currency}/today/?format=json'
-    source_response = requests.get(source_url)
-    source_json_data = json.loads(source_response.text)
+        source_response = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/c/{source_currency}/today/?format=json')
+        source_json_data = json.loads(source_response.text)
 
-    target_currency_url = f'https://api.nbp.pl/api/exchangerates/rates/c/{target_currency}/today/?format=json'
-    target_response = requests.get(target_currency_url)
-    target_json_data = json.loads(target_response.text)
+        target_response = requests.get(f'https://api.nbp.pl/api/exchangerates/rates/c/{target_currency}/today/?format=json')
+        target_json_data = json.loads(target_response.text)
 
-    scaler = source_json_data['rates'][0]['ask']/target_json_data['rates'][0]['ask']
-    return round(source_currency_amount * scaler, 2)
-#print(converter())
+        scaler = source_json_data['rates'][0]['ask']/target_json_data['rates'][0]['ask']
+        return round(source_currency_amount * scaler, 2)
+    except:
+        return 'Number was not typed or invalid shortcut'
 
+def exchange_rate_chart():
+    try:
+        currency = input('Enter currency you want to check (GBP, PLN, EUR etc.): ')
+        period_length = int(input('Enter period length: '))
+        response = requests.get(f'http://api.nbp.pl/api/exchangerates/rates/a/{currency}/last/{period_length}/?format=json')
+        data = json.loads(response.text)
+        kursy = []
+        dates = []
+        for i in range(len(data['rates'])):
+            kursy.append(data['rates'][i]['mid'])
+            dates.append(data['rates'][i]['effectiveDate'])
+        plt.plot(dates, kursy)
+        plt.ylabel('Exchange rate')
+        plt.xlabel('Date')
+        plt.show()
+    except:
+        return 'Number was not typed or invalid shortcut'
 
-#ask_sum = 0
-#bid_sum = 0
-#for value in json_data['rates']:
-#    ask_sum += value['ask']
-#   bid_sum += value['bid']
-#print(round(ask_sum/5, 2))
-#print(json_data['rates'])
+if option_choose == 1:
+    print(rate_check())
+elif option_choose == 2:
+    print(converter())
+elif option_choose == 3:
+    print(exchange_rate_chart())
--- idkco.py
+++
@@ -1,8 +0,0 @@
-import requests
-from pprint import pprint
-
-API_key = '5d54440abaa032b7a65ea578b2a3bfe0'
-city = input('Enter city name: ')
-url = f'https://api.openweathermap.org/data/2.5/weather?q=London&APPID={API_key}&units=metric'
-response = requests.get(url).json()
-print(response)
